8aa7b936820befa40c8ae5bbbda0be94e0ad83c2
night-crawler <lilo.panic@gmail.com>
2019-01-17 17:30:57 +0300

    wip

--- cldr_language_helpers/annotator.py
+++ cldr_language_helpers/annotator.py
@@ -1,4 +1,8 @@
+import re
+import typing as t
 import json
+from collections import defaultdict
+from functools import reduce, lru_cache
 
 from . import conf
 
@@ -26,41 +30,139 @@ TYPE_BY_CHAR_MAP = {
     for char, lang_indexes
     in __uchar_to_lang_map['char_map'].items()
 }
+ALL_LANGUAGES = set(
+    li.split('::')[0]
+    for li in __uchar_to_lang_map['lang_index'].keys()
+)
 
 __uchar_to_lang_map = None
 __lang_index = None
+__sentinel = object()
+
+
+def get_possible_langs_by_char(char):
+    langs_set = LANG_BY_CHAR_MAP.get(char, __sentinel)
+    if langs_set is not __sentinel:
+        return langs_set
+
+    if re.match(r'\s', char):
+        return {'space'}
+
+    return {'unknown'}
+
+
+def get_possible_char_types_by_char(char):
+    char_types = TYPE_BY_CHAR_MAP.get(char, __sentinel)
+    if char_types is not __sentinel:
+        return char_types
+
+    if re.match(r'\s', char):
+        return {'space'}
+
+    return {'unknown'}
 
 
 class StringAnnotator(str):
     @property
-    def langs_by_index(self):
-        return [
-            LANG_BY_CHAR_MAP.get(char, set())
-            for char in self
-        ]
+    @lru_cache(None)
+    def langs_by_index(self) -> t.List[t.Set[str]]:
+        return [get_possible_langs_by_char(char) for char in self]
 
     @property
-    def char_types_by_index(self):
-        return [
-            TYPE_BY_CHAR_MAP.get(char, set())
-            for char in self
-        ]
+    @lru_cache(None)
+    def char_types_by_index(self) -> t.List[t.Set[str]]:
+        return [get_possible_char_types_by_char(char) for char in self]
 
-    def get_lang_intersection(self):
+    @property
+    @lru_cache(None)
+    def all_langs(self) -> t.Set[str]:
+        accum = set()
+        for lbi in self.langs_by_index:
+            accum |= lbi
+        return accum
+
+    @property
+    @lru_cache(None)
+    def langs_intersection(self):
         return set.intersection(*self.langs_by_index)
 
-    def get_char_type_intersection(self):
+    @property
+    @lru_cache(None)
+    def char_types_intersection(self):
         return set.intersection(*self.char_types_by_index)
 
-    def _split_by_lang_intersection(self):
+    @property
+    @lru_cache(None)
+    def lang_stats(self) -> t.Dict[str, int]:
+        stats = defaultdict(lambda: 0)
+        for lbi in self.langs_by_index:
+            for lang in lbi:
+                stats[lang] += 1
+        return stats
+
+    def has_langs(self, *langs) -> bool:
+        if not self:
+            return False
+        return set(langs).issubset(self.all_langs)
+
+    def has_langs_throughout(self, *langs) -> bool:
+        if not self:
+            return False
+
+        langs = set(langs)
+        for lbi in self.langs_by_index:
+            if lbi == {'space'}:
+                continue
+
+            if not langs.issubset(lbi):
+                return False
+        return True
+
+    def _split_by_char_type_ranges(self):
+        if not self:
+            return
+
+        def reducer(state, item):
+            pos, char_types = item
+
+            if 'numbers' in char_types:
+                char_type = 'numbers'
+            elif {'main', 'index', 'auxiliary'} & char_types:
+                char_type = 'regular'
+            elif 'punctuation' in char_types:
+                char_type = 'punctuation'
+            elif 'space' in char_types:
+                char_type = 'space'
+            else:
+                char_type = 'unknown'
+
+            if not state:
+                return [[pos, pos + 1, char_type]]
+
+            if state[-1][2] == char_type:
+                state[-1][1] = pos + 1
+                return state
+            else:
+                state.append([pos, pos + 1, char_type])
+                return state
+
+        return reduce(reducer, enumerate(self.char_types_by_index), [])
+
+    def iter_split_by_char_type(self):
+        for start, stop, _type in self._split_by_char_type_ranges():
+            yield self[start:stop]
+
+    def split_by_char_type(self):
+        return list(self.iter_split_by_char_type())
+
+    def _split_by_lang_intersection_ranges(self):
         if not self:
             return
 
         start_pos = 0
-        langs_by_index = self.langs_by_index
-        langs_intersection = langs_by_index[0]
+        langs_intersection = self.langs_by_index[0]
 
-        for pos, langs_set in enumerate(langs_by_index):
+        for pos, langs_set in enumerate(self.langs_by_index):
             langs_intersection = langs_intersection.intersection(langs_set)
             if not langs_intersection and pos:
                 yield start_pos, pos
@@ -70,7 +172,7 @@ class StringAnnotator(str):
         yield start_pos, len(self)
 
     def iter_split_by_lang_intersection(self):
-        for start, stop in self._split_by_lang_intersection():
+        for start, stop in self._split_by_lang_intersection_ranges():
             yield self[start:stop]
 
     def split_by_lang_intersection(self):
--- tests/annotator_test.py
+++ tests/annotator_test.py
@@ -13,15 +13,39 @@ class StringAnnotatorTest:
     def test_used_languages(self):
         assert 'ru_RU' in StringAnnotator('ф').langs_by_index[0]
 
+    def test_langs(self):
+        assert {'ru_RU', 'en_US', 'en', 'ru'}.issubset(StringAnnotator('йцу 123 qwe LOL').all_langs)
+
+    def test_lang_stats(self):
+        stats = StringAnnotator('qwe йцу').lang_stats
+        assert stats['ru_RU'] == 3
+        assert stats['ru'] == 3
+        assert stats['en'] == 3
+        assert stats['space'] == 1
+
     def test_get_lang_intersection(self):
         sa = StringAnnotator('somesortof123')
-        assert 'en' in sa.get_lang_intersection()
+        assert 'en' in sa.langs_intersection
 
     def test_get_char_type_intersection(self):
-        assert StringAnnotator('somesortof123').get_char_type_intersection() == set()
-        assert StringAnnotator('somesortof').get_char_type_intersection() == {'auxiliary', 'main'}
-        assert StringAnnotator(' ').get_char_type_intersection() == set()
+        assert StringAnnotator('somesortof123').char_types_intersection == set()
+        assert StringAnnotator('somesortof').char_types_intersection == {'auxiliary', 'main'}
+        assert StringAnnotator(' ').char_types_intersection == {'space'}
 
-    def test__split_by_lang_intersection(self):
-        s = list(StringAnnotator('что-то everything как-то lol !').split_by_lang_intersection())
-        print(s)
+    def test_split_by_lang_intersection(self):
+        assert StringAnnotator('что-то everything как-то lol !').split_by_lang_intersection() == \
+               ['что-то', ' ', 'everything', ' ', 'как-то', ' ', 'lol', ' ', '!']
+
+        assert StringAnnotator('somesortof123').split_by_lang_intersection() == ['somesortof123']
+
+    def test_split_by_char_type(self):
+        assert StringAnnotator('qwe, 123!!!').split_by_char_type() == \
+               ['qwe', ',', ' ', '123', '!!!']
+
+    def test_has_langs(self):
+        assert StringAnnotator('йцу 123 qwe LOL').has_langs('ru', 'en')
+
+    def test_has_langs_throughout(self):
+        assert StringAnnotator().has_langs_throughout('ru') is False
+        assert StringAnnotator('йцу 123').has_langs_throughout('ru') is True
+        assert StringAnnotator('йцу 123').has_langs_throughout('ru_RU') is True
